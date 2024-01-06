f23aef48c452dd6759e0cbee7d99aab14724fda3
sunxen <sunxen@qq.com>
2024-01-06 15:39:36 +0800

    Responsive css

--- packages/nodejs-docsearch/src/App.tsx
+++ packages/nodejs-docsearch/src/App.tsx
@@ -2,13 +2,15 @@ import { DocSearch } from '@docsearch/react';
 import '@docsearch/css';
 import { createRoot } from 'react-dom/client';
 
+import './App.css';
+
 export default function App() {
   return (
     <div>
       <DocSearch
-        indexName="docsearch"
-        appId="R2IYF7ETH7"
-        apiKey="599cec31baffa4868cae4e79f180729b"
+        indexName=""
+        appId=""
+        apiKey=""
       />
     </div>
   );
@@ -17,11 +19,8 @@ export default function App() {
 export function renderSearchButton() {
   // add div to top right of page, then add button to div
   const div = document.createElement('div');
-  div.style.position = 'fixed';
-  div.style.top = '0';
-  div.style.right = '0';
-  div.style.zIndex = '99';
-  document.body.appendChild(div);
+  div.id = 'nodejs-docsearch-container';
+  (document.querySelector('header') || document.body).appendChild(div);
   const root = createRoot(div);
   root.render(<App />);
 }
