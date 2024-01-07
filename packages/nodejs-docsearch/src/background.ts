25d43c8268696a7324cc8b970a1010086585e418
sunxen <sunxen@qq.com>
2024-01-07 16:46:46 +0800

    Add permissions and update URL matches in manifest.json, refactor background.ts and content.ts

--- packages/nodejs-docsearch/src/background.ts
+++ packages/nodejs-docsearch/src/background.ts
@@ -1,5 +1,35 @@
 chrome.action.onClicked.addListener((tab) => {
-  console.log('用户点击了插件图标2');
-  if (!tab.id) return;
-  chrome.tabs.sendMessage(tab.id, { action: 'toggleSearchDialog' });
+  if (!tab.id || tab.url?.startsWith('chrome://')) {
+    openDoc();
+    return;
+  };
+
+  // check page content has "div.id = 'nodejs-docsearch-container';"
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: () => {
+      return document.getElementById('nodejs-docsearch-container');
+    }
+  }).then((result) => {
+    if (result[0].result) {
+      toggleSearchModal(tab.id!);
+    } else {
+      openDoc();
+    }
+  });
 });
+
+
+function toggleSearchModal(tabId: number) {
+  chrome.scripting.executeScript({
+    target: { tabId },
+    func: () => {
+      // packages/docsearch-react/src/useDocSearchKeyboardEvents.ts
+      window.dispatchEvent(new Event('toggleSearchModal'));
+    }
+  });
+}
+
+function openDoc() {
+  chrome.tabs.create({ url: 'https://nodejs.org/api/' })
+}
--- packages/nodejs-docsearch/src/content.ts
+++ packages/nodejs-docsearch/src/content.ts
@@ -1,11 +1,3 @@
 import { renderSearchButton } from "./App";
 
 renderSearchButton();
-
-// document.addEventListener('toggleSearchDialog', showSearchDialog)
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'toggleSearchDialog') {
-    window.dispatchEvent(new Event('toggleSearchModal'));
-  }
-});
-
